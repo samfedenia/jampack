00f81b494aa53cb9a65d7c0951b92a4dc9d0ab67
Sam Fedenia <sam@fedenia.xyz>
2021-05-10 12:59:20 -0500

    add controlled forms

--- client/src/forms/SignupForm.jsx
+++ client/src/forms/SignupForm.jsx
@@ -14,44 +14,85 @@ const style = {
   },
   textField: {
     width: '50ch',
+    margin: '8px',
   },
   button: {
     margin: '1rem',
     width: '10rem',
     backgroundColor: 'green',
   },
+  form: {
+    display: 'flex',
+    flexWrap: 'no-wrap',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '1rem',
+    marginBottom: '1rem',
+  },
 };
 
 export default function SignupForm() {
   const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [data, setData] = useState({ email: '', password: '' });
 
   function toggleButton() {
     setButtonDisabled(!buttonDisabled);
   }
+  function onChange(ev) {
+    setData({ ...data, [ev.target.name]: ev.target.value });
+  }
+
+  function onSubmit(ev) {
+    ev.preventDefault();
+    console.log('email', data.email, 'password', data.password);
+    setData({ ...data, email: '', password: '' });
+  }
+
   return (
     <div style={style.root}>
-      <div>
-        <Typography variant='h4'>Create an account:</Typography>
-      </div>
-      <div>
-        <TextField label='Email' id='margin-none' style={style.textField} />
-      </div>
-      <div>
-        <TextField label='Password' id='margin-none' style={style.textField} />
-      </div>
-      <div>
-        <TermsCheckbox toggleButton={toggleButton} />
-      </div>
-      <div>
-        <Button
-          disabled={buttonDisabled}
-          variant='contained'
-          color='secondary'
-          style={style.button}
-        >
-          Sign up
-        </Button>
-      </div>
+      <form onSubmit={onSubmit} autoComplete='off' style={style.form}>
+        <div>
+          <Typography variant='h4'>Create an account:</Typography>
+        </div>
+
+        <div>
+          <TextField
+            label='Email'
+            id='margin-none'
+            style={style.textField}
+            onChange={onChange}
+            name='email'
+            type='email'
+            value={data.email}
+          />
+        </div>
+        <div>
+          <TextField
+            label='Password'
+            id='margin-none'
+            style={style.textField}
+            onChange={onChange}
+            name='password'
+            type='password'
+            value={data.password}
+          />
+        </div>
+        <div>
+          <TermsCheckbox toggleButton={toggleButton} />
+        </div>
+        <div>
+          <Button
+            disabled={buttonDisabled}
+            variant='contained'
+            color='secondary'
+            style={style.button}
+            type='submit'
+          >
+            Sign up
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
