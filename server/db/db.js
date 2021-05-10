9e4688cb60e06d6751a1e03d50a4056ea5ff9738
Sam Fedenia <sam@fedenia.xyz>
2021-05-10 00:37:17 -0500

    added react

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,10 @@
 const Sequelize = require('sequelize');
 const DATABASE_URL =
   process.env.DATABASE_URL || 'postgres://localhost/packplanner_db';
-const db = new Sequelize(DATABASE_URL);
+const dbConfig = {
+  logging: false,
+};
+
+const db = new Sequelize(DATABASE_URL, dbConfig);
 
 module.exports = db;
