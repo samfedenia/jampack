1b96a68cb289917e251655fae0b6e82267336786
Sam Fedenia <sam@fedenia.xyz>
2021-05-10 03:27:55 -0500

    added navbar

--- server/app.js
+++ server/app.js
@@ -10,10 +10,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.use('/api', router);
+
+app.get('*', (req, res) => {
   res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-app.use('/api', router);
 
 module.exports = app;
