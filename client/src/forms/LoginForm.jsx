46abca57bcf40a1758d0f9385d3e3cef8fc4fef2
Sam Fedenia <sam@fedenia.xyz>
2021-05-10 20:27:06 -0500

    fixed inf loop on useEffect

--- client/src/forms/LoginForm.jsx
+++ client/src/forms/LoginForm.jsx
@@ -58,7 +58,7 @@ export default function LoginForm() {
     } catch (ex) {
       console.log(ex);
     }
-  }, [error, user]);
+  }, []);
 
   const [data, setData] = useState({ email: '', password: '' });
 
--- client/src/store/reducers/itemsReducer.js
+++ client/src/store/reducers/itemsReducer.js
@@ -1,9 +1,14 @@
 import { GET_ITEMS } from '../actions/items/getItems';
+import { LOGOUT_USER } from '../actions/user/logout';
 
 const itemsReducer = (state = [], action) => {
   if (action.type === GET_ITEMS) {
     console.log(action.items);
     return [...state, ...action.items];
+  }
+  if (action.type === LOGOUT_USER) {
+    console.log(action.items);
+    return [];
   }
   return state;
 };
